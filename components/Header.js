d592929cfd7fe1e1915008dde30bf310162a6fad
Partha Sen <partha1189@gmail.com>
2021-09-13 19:52:31 +0530

    first

---
+++ components/Header.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
+
+const Header = () => {
+  return (
+    <header className="sticky top- z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      {/* left */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      {/* middle --search */}
+      <div className="flex items-center md:border-2 rounded-full justify-between py-2 md:shadow-sm">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
+
+      {/* right */}
+      <div className="flex items-center justify-end text-gray-500 space-x-4">
+        <p>Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex border-2 rounded-full items-center space-x-2 p-2">
+          <MenuIcon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
